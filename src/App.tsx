00d3d1dbe3b85c6ef63faec97b472b8504fb0c7c
SaltySalt77 <hyna0707@gmail.com>
2024-11-18 00:16:37 +0900

    Docs : 리드미에 커밋컨벤션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import "./App.css";
-import tz from "dayjs/plugin/timezone";
-import utc from "dayjs/plugin/utc";
-import dayjs from "dayjs";
-import { locale } from "dayjs";
-import "dayjs/locale/ko";
+import './App.css';
+import tz from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+import dayjs from 'dayjs';
+import { locale } from 'dayjs';
+import 'dayjs/locale/ko';
 
 function App() {
-  dayjs.extend(utc);
-  dayjs.extend(tz);
-  locale("ko");
-  dayjs.tz.setDefault("Asia/Seoul");
-  return <div>HelloWorld!</div>;
+    dayjs.extend(utc);
+    dayjs.extend(tz);
+    locale('ko');
+    dayjs.tz.setDefault('Asia/Seoul');
+    return <div>HelloWorld!</div>;
 }
 
 export default App;
