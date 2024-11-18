6ec1311076eca6fc837c56b41ce7a9e9f5148b8c
LBB <202780@jnu.ac.kr>
2024-11-19 01:16:22 +0900

    Feat : Search 아이콘 추가

---
+++ src/components/Icons/SearchIcon.tsx
@@ -0,0 +1,27 @@
+import { createSvgIcon } from '@mui/material';
+
+const SearchIcon = createSvgIcon(
+    <svg viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <g id='Group 10'>
+            <path
+                id='Vector'
+                d='M9.88889 18.7778C14.7981 18.7778 18.7778 14.7981 18.7778 9.88889C18.7778 4.97969 14.7981 1 9.88889 1C4.97969 1 1 4.97969 1 9.88889C1 14.7981 4.97969 18.7778 9.88889 18.7778Z'
+                stroke='currentColor'
+                strokeWidth={2}
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+            <path
+                id='Vector_2'
+                d='M21.0001 20.9999L16.1667 16.1666'
+                stroke='currentColor'
+                strokeWidth={2}
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+        </g>
+    </svg>,
+    'SearchIcon'
+);
+
+export default SearchIcon;
