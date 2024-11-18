1940563762ce5aba7f5dca96ec1a270ce1a89138
LBB <202780@jnu.ac.kr>
2024-11-19 01:39:29 +0900

    Feat : Header 컴포넌트 생성

---
+++ src/components/Header.style.ts
@@ -0,0 +1,10 @@
+import { SxProps, Theme } from '@mui/material';
+
+export const container: SxProps<Theme> = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '24px 16px',
+    width: '100%',
+    boxSizing: 'border-box',
+};
