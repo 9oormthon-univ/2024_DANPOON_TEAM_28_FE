30369335197e19969249200641ead514fa674cec
LBB <202780@jnu.ac.kr>
2024-11-19 01:03:59 +0900

    Feat : Logo 컴포넌트 생성

---
+++ src/components/Logo.tsx
@@ -0,0 +1,32 @@
+import logo_only_sm from '@/assets/logo_only_sm.svg';
+import logo_only_md from '@/assets/logo_only_md.svg';
+import logo_only_lg from '@/assets/logo_only_lg.svg';
+import logo_lively_sm from '@/assets/logo_lively_sm.svg';
+import logo_lively_lg from '@/assets/logo_lively_lg.svg';
+
+interface ILogoProps {
+    size?: 'small' | 'medium' | 'large';
+    showName?: boolean;
+};
+
+const Logo = ({ size = 'small', showName = true }: ILogoProps) => {
+    const getLogoSrc = () => {
+        if (showName) {
+            return size === 'small' ? logo_lively_sm : logo_lively_lg;
+        } else {
+            switch (size) {
+                case 'small':
+                    return logo_only_sm;
+                case 'medium':
+                    return logo_only_md;
+                case 'large':
+                default:
+                    return logo_only_lg;
+            }
+        }
+    };
+
+    return <img src={getLogoSrc()} alt='Logo' />;
+};
+
+export default Logo;
