6980ee50c3aff385aaaabb4cb179e95c5b2d1595
LBB <202780@jnu.ac.kr>
2024-11-19 01:13:09 +0900

    Feat : Notification 아이콘 추가

---
+++ src/components/Icons/NotificationIcon.tsx
@@ -0,0 +1,23 @@
+import { createSvgIcon } from '@mui/material';
+
+const NotificationIcon = createSvgIcon(
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 18' fill='none'>
+        <path
+            d='M13.3333 5.80085C13.3333 4.52759 12.7714 3.30647 11.7712 2.40614C10.771 1.5058 9.41449 1 8 1C6.58551 1 5.22896 1.5058 4.22876 2.40614C3.22857 3.30647 2.66667 4.52759 2.66667 5.80085C2.66667 11.4018 1 13.0021 1 13.0021H15C15 13.0021 13.3333 11.4018 13.3333 5.80085Z'
+            stroke='#57873E'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M9.53777 16.2026C9.3815 16.4451 9.15719 16.6464 8.88731 16.7864C8.61743 16.9263 8.31144 16.9999 8 16.9999C7.68855 16.9999 7.38257 16.9263 7.11268 16.7864C6.8428 16.6464 6.6185 16.4451 6.46222 16.2026'
+            stroke='#57873E'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>,
+    'NotificationIcon'
+);
+
+export default NotificationIcon;
