b6379c791c361e9a323621b19cfb02b22f23dd70
LBB <202780@jnu.ac.kr>
2024-11-19 02:44:47 +0900

    Feat : 지도, 마이페이지 아이콘 추가

---
+++ src/components/Icons/MyPageIcon.tsx
@@ -0,0 +1,30 @@
+import { createSvgIcon } from '@mui/material';
+
+const MyPageIcon = createSvgIcon(
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='22'
+        height='22'
+        viewBox='0 0 22 22'
+        fill='none'
+    >
+        <path
+            d='M21 21V18.7401C21 17.5414 20.4732 16.3918 19.5355 15.5441C18.5979 14.6965 17.3261 14.2203 16 14.2203H6C4.67392 14.2203 3.40215 14.6965 2.46447 15.5441C1.52678 16.3918 1 17.5414 1 18.7401V21'
+            stroke='currentColor'
+            strokeOpacity='1'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M11 11C13.7614 11 16 8.76142 16 6C16 3.23858 13.7614 1 11 1C8.23858 1 6 3.23858 6 6C6 8.76142 8.23858 11 11 11Z'
+            stroke='currentColor'
+            strokeOpacity='1'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>,
+    'MyPageIcon'
+);
+export default MyPageIcon;
