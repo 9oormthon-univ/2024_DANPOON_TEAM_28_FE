5a5daf9407e6c9dfdf8d41df19afee90eff0ec03
SaltySalt77 <hyna0707@gmail.com>
2024-11-19 21:19:10 +0900

    Feat : 알림 내용 부분 추가
    알림 아이콘 추가 필요

--- src/pages/alarm/Alarm.tsx
+++ src/pages/alarm/Alarm.tsx
@@ -1,7 +1,29 @@
 import { IAlarmProps } from '@/types/IAlarmProps';
+import { ListItem, ListItemText, Typography } from '@mui/material';
 
 const Alarm = ({ title, subtitle, pushedAt, type, read }: IAlarmProps) => {
-    return <div></div>;
+    return (
+        <ListItem>
+            <ListItemText
+                primary={
+                    <>
+                        <Typography>{title}</Typography>
+                        {subtitle ?? (
+                            <Typography
+                                sx={{
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                }}
+                            >
+                                {subtitle}
+                            </Typography>
+                        )}
+                    </>
+                }
+                secondary={pushedAt}
+            />
+        </ListItem>
+    );
 };
 
 export default Alarm;
