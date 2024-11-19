baa2ddf6db6c2fad75f2d0818a6c51b82fceea85
SaltySalt77 <hyna0707@gmail.com>
2024-11-19 20:46:27 +0900

    Feat : Alarm 컴포넌트 임시 추가

---
+++ src/pages/alarm/Alarm.tsx
@@ -0,0 +1,7 @@
+import { IAlarmProps } from '@/types/IAlarmProps';
+
+const Alarm = ({ title, subtitle, pushedAt, type, read }: IAlarmProps) => {
+    return <div></div>;
+};
+
+export default Alarm;
