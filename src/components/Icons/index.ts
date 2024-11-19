b132624b93e2c05444c7dde6db6b489d999c7dbc
SaltySalt77 <hyna0707@gmail.com>
2024-11-19 20:27:20 +0900

    Feat : index.ts에 빠진 아이콘 추가

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,2 +1,7 @@
+export { default as CheckIcon } from './CheckIcon';
+export { default as DocsIcon } from './DocsIcon';
 export { default as NotificationIcon } from './NotificationIcon';
+export { default as PersonIcon } from './PersonIcon';
 export { default as SearchIcon } from './SearchIcon';
+export { default as StatusIcon } from './StatusIcon';
+export { default as TrophyIcon } from './TrophyIcon';
