5d38e1722c40cdd7e16ca1ae1f851739defbcbda
SaltySalt77 <hyna0707@gmail.com>
2024-11-19 21:57:48 +0900

    Design : status 아이콘 수정

--- src/components/Icons/StatusIcon.tsx
+++ src/components/Icons/StatusIcon.tsx
@@ -3,26 +3,82 @@ import { createSvgIcon } from '@mui/material';
 const StatusIcon = createSvgIcon(
     <svg
         xmlns='http://www.w3.org/2000/svg'
-        width='20'
+        width='48'
         height='48'
-        viewBox='0 0 20 48'
+        viewBox='0 0 48 48'
         fill='none'
     >
         <path
-            d='M0 36V21'
-            stroke='currentColor'
-            strokeOpacity='1'
-            strokeWidth='2.5'
-            strokeLinecap='round'
-            strokeLinejoin='round'
+            d='M28 36V21'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
         />
         <path
-            d='M7.99988 36V15.75'
-            stroke='currentColor'
-            strokeOpacity='1'
-            strokeWidth='2.5'
-            strokeLinecap='round'
-            strokeLinejoin='round'
+            d='M36 36V15.75'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
+        />
+        <path
+            d='M20 36V12'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
+        />
+        <path
+            d='M12 36V27'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
+        />
+        <path
+            d='M28 36V21'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
+        />
+        <path
+            d='M36 36V15.75'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
+        />
+        <path
+            d='M20 36V12'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
+        />
+        <path
+            d='M12 36V27'
+            style={{
+                stroke: 'currentColor',
+                strokeWidth: 2.5,
+                strokeLinecap: 'round',
+                strokeLinejoin: 'round',
+            }}
         />
     </svg>,
     'StatusIcon'
