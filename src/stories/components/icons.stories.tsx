b9982ec908c2b6f6a82ea5be3283feca540c1c3c
SaltySalt77 <hyna0707@gmail.com>
2024-11-19 20:29:31 +0900

    Docs : 스토리북에 추가한 아이콘 추가

--- src/stories/components/icons.stories.tsx
+++ src/stories/components/icons.stories.tsx
@@ -1,5 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { NotificationIcon, SearchIcon } from '@/components/Icons';
+import {
+    CheckIcon,
+    DocsIcon,
+    NotificationIcon,
+    PersonIcon,
+    SearchIcon,
+    StatusIcon,
+    TrophyIcon,
+} from '@/components/Icons';
 
 const meta: Meta = {
     title: 'components/Icons',
@@ -10,6 +18,20 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+export const Check: Story = {
+    args: {
+        component: CheckIcon,
+    },
+    render: (args) => <args.component sx={{ color: 'primary.main' }} />,
+};
+
+export const Docs: Story = {
+    args: {
+        component: DocsIcon,
+    },
+    render: (args) => <args.component sx={{ color: 'primary.main' }} />,
+};
+
 export const Notification: Story = {
     args: {
         component: NotificationIcon,
@@ -17,9 +39,30 @@ export const Notification: Story = {
     render: (args) => <args.component sx={{ color: 'primary.main' }} />,
 };
 
+export const Person: Story = {
+    args: {
+        component: PersonIcon,
+    },
+    render: (args) => <args.component sx={{ color: 'primary.main' }} />,
+};
+
 export const Search: Story = {
     args: {
         component: SearchIcon,
+    },
+    render: (args) => <args.component sx={{ color: 'primary.main' }} />,
+};
+
+export const Status: Story = {
+    args: {
+        component: StatusIcon,
+    },
+    render: (args) => <args.component sx={{ color: 'primary.main' }} />,
+};
+
+export const Trophy: Story = {
+    args: {
+        component: TrophyIcon,
     },
     render: (args) => <args.component sx={{ color: 'primary.main' }} />,
 };
