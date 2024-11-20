0c76bfb087e6d46a5cb9593c942b744f89d346b2
SaltySalt77 <hyna0707@gmail.com>
2024-11-20 22:10:34 +0900

    Docs : storybook에 Layout 추가

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,6 +2,13 @@ import BottomNav from '@/components/BottomNav';
 import { Container, SxProps } from '@mui/material';
 import React from 'react';
 
+export interface ILayoutProps {
+    children: React.ReactNode;
+    sx?: SxProps;
+    removePadding?: boolean;
+    removeBottomNavigation?: boolean;
+}
+
 const Layout = ({
     children,
     sx,
@@ -15,11 +22,12 @@ const Layout = ({
 }) => {
     return (
         <Container
-            maxWidth={'md'}
+            maxWidth={'sm'}
             sx={{
                 overflowX: 'hidden',
                 backgroundColor: 'background.default',
                 px: removePadding ? 0 : 2,
+                position: 'relative',
                 ...sx,
             }}
             disableGutters
