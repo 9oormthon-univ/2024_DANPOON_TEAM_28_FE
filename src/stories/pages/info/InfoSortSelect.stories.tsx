2116c38250dc736b7c65ac8857fb9b21541116a2
LBB <202780@jnu.ac.kr>
2024-11-22 04:38:23 +0900

    Design : InfoSortSelect 컴포넌트 디자인 수정

--- src/stories/pages/info/InfoSortSelect.stories.tsx
+++ src/stories/pages/info/InfoSortSelect.stories.tsx
@@ -2,29 +2,13 @@ import { Meta, StoryObj } from '@storybook/react';
 import InfoSortSelect from '@/pages/info/InfoSortSelect';
 
 const meta: Meta = {
-  title: 'Info/InfoSortSelect', // Storybook 탐색 패널에서 보이는 이름
+  title: 'Info/InfoSortSelect',
   component: InfoSortSelect,
-  tags: ['autodocs'], // 자동 문서화를 위한 태그
-  argTypes:{
-    
-  }
+  tags: ['autodocs'],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof InfoSortSelect>;
 
-// 기본 상태의 스토리
-export const LatestOrder: Story = {
-  args: {
-    value:'최신순',
-  }
-};
-
-// 선택 상태를 초기화한 스토리
-export const PopularOrder: Story = {
-  args: {
-    // 초기 값을 "인기순"으로 설정하기 위해 props를 전달
-    value: '인기순',
-  },
-};
+export const Primary: Story = {};
