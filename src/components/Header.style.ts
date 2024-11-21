35b1384759dea47e347fb1077f3d1c7223411cff
SaltySalt77 <hyna0707@gmail.com>
2024-11-21 13:17:04 +0900

    Design : 헤더 높이 고정 및 알림 버튼 크기 줄임

--- src/components/Header.style.ts
+++ src/components/Header.style.ts
@@ -7,4 +7,11 @@ export const container: SxProps<Theme> = {
     padding: '24px 16px',
     width: '100%',
     boxSizing: 'border-box',
+    height: '4.25rem',
+};
+
+export const notificationButton: SxProps<Theme> = {
+    color: 'livelyPrimary.2',
+    height: '1rem',
+    padding: 0,
 };
