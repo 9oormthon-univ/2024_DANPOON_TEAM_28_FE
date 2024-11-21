8d9033e434d1a70f493c150a5656b29540bd0461
LBB <202780@jnu.ac.kr>
2024-11-22 05:05:38 +0900

    Feat : constants에 InfoCard에 필요한 데이터 추가

---
+++ src/constants/infoCardsData.tsx
@@ -0,0 +1,45 @@
+import info_img_1 from '@/assets/info_card_img_1.png';
+import info_img_2 from '@/assets/info_card_img_2.png';
+import info_img_3 from '@/assets/info_card_img_3.png';
+import info_img_4 from '@/assets/info_card_img_4.png';
+import info_img_5 from '@/assets/info_card_img_5.png';
+import info_img_6 from '@/assets/info_card_img_6.png';
+
+export const infoCardsData = [
+    {
+        title: '서울특별시 2024년「고립 은둔 청년 지원 사업」',
+        date: new Date('2024-01-01'),
+        view: 5203,
+        image: info_img_1,
+    },
+    {
+        title: '청년재단 「청년 체인지업 프로젝트」 모집',
+        date: new Date('2024-02-02'),
+        view: 2035,
+        image: info_img_2,
+    },
+    {
+        title: '실내에서 키우기 쉬운 식물 10가지',
+        date: new Date('2024-03-03'),
+        view: 3502,
+        image: info_img_3,
+    },
+    {
+        title: '마음 챙김 명상을 왜 해야 하나요? 6가지 명상 효과',
+        date: new Date('2024-04-04'),
+        view: 3250,
+        image: info_img_4,
+    },
+    {
+        title: '라이블리 Pick! 서울 근교 조용한 카페 추천',
+        date: new Date('2024-05-05'),
+        view: 2530,
+        image: info_img_5,
+    },
+    {
+        title: '서울식물원 청년 힐링 프로그램 모집',
+        date: new Date('2024-06-06'),
+        view: 5032,
+        image: info_img_6,
+    },
+];
