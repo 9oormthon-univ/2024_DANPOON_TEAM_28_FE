cd27f7b1548b03c2fbee9c0cb2b9352d713e0b93
LBB <202780@jnu.ac.kr>
2024-11-22 14:07:51 +0900

    Design : InfoCategoryTabs 스타일 수정

--- src/pages/info/InfoCategoryTabs.style.ts
+++ src/pages/info/InfoCategoryTabs.style.ts
@@ -1,19 +1,29 @@
-import { SxProps } from '@mui/system';
+import { SxProps, Theme } from '@mui/system';
 
 export const containerStyle: SxProps = {
     width: '100%',
     maxWidth: '100%',
     backgroundColor: 'white',
     '& .MuiTabs-flexContainer': {
-      paddingLeft: '5px',
-      paddingRight: '5px',
+        paddingLeft: '5px',
+        paddingRight: '5px',
     },
 };
+
+export const labelStyle: SxProps<Theme> = (theme) => ({
+    fontSize: '0.75rem',
+    [theme.breakpoints.up('sm')]: {
+        fontSize: '1.2rem',
+    },
+    [theme.breakpoints.up(360)]: {
+        fontSize: '0.9rem',
+    },
+});
 
 export const tabsStyle: SxProps = {
     borderBottom: 2,
     borderColor: 'divider',
-  justifyContent: 'space-between',
+    justifyContent: 'space-between',
     width: '100%',
     '& .MuiTab-root': {
         minWidth: 0,
--- src/pages/info/InfoCategoryTabs.tsx
+++ src/pages/info/InfoCategoryTabs.tsx
@@ -1,59 +1,95 @@
-import * as React from 'react';
-import {Tabs, Tab, Box, Typography} from '@mui/material';
+import { useState } from 'react';
+import { Tabs, Tab, Box, Typography } from '@mui/material';
 import * as style from './InfoCategoryTabs.style';
+import InfoTabPanelContent from './InfoTabPanelContent';
+import InfoSortSelect from './InfoSortSelect';
 
-const tabLabels = ['지원사업', '마음챙김', '위로의 글', '생활습관', '방문지 추천'];
+const tabLabels = [
+    '지원사업',
+    '마음챙김',
+    '위로의 글',
+    '생활습관',
+    '방문지 추천',
+];
 
 interface ITabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+    children?: React.ReactNode;
+    index: number;
+    value: number;
 }
 
 function TabPanel(props: ITabPanelProps) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      aria-labelledby={`tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{...style.tabPanelStyle}}>{children}</Box>}
-    </div>
-  );
+    return (
+        <div
+            role='tabpanel'
+            hidden={value !== index}
+            id={`tabpanel-${index}`}
+            aria-labelledby={`tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ ...style.tabPanelStyle }}>{children}</Box>
+            )}
+        </div>
+    );
 }
 
 export default function InfoCategoryTabs() {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = useState(0);
+    const [sortOption, setSortOption] = useState<string>('latest');
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Box sx={{...style.containerStyle}}>
-      <Box sx={{ ...style.tabsStyle }}>
-        <Tabs value={value} variant="fullWidth" onChange={handleChange} aria-label="info tabs" >
-        {tabLabels.map((label, index) => (
-            <Tab
-              key={index}
-              label={
-                <Typography variant={value === index ? 'body3SemiBold' : 'body3Regular'}>
-                  {label}
-                </Typography>
-              }
+    const handleSortOptionChange = (option: string) => {
+        setSortOption(option);
+    };
+
+    return (
+        <Box sx={style.containerStyle}>
+            <Box sx={style.tabsStyle}>
+                <Tabs
+                    value={value}
+                    variant='fullWidth'
+                    onChange={handleTabChange}
+                    aria-label='info tabs'
+                >
+                    {tabLabels.map((label, index) => (
+                        <Tab
+                            key={index}
+                            label={
+                                <Typography
+                                    sx={style.labelStyle}
+                                    variant={
+                                        value === index
+                                            ? 'body3SemiBold'
+                                            : 'body3Regular'
+                                    }
+                                >
+                                    {label}
+                                </Typography>
+                            }
+                        />
+                    ))}
+                </Tabs>
+            </Box>
+
+            <InfoSortSelect
+                sortOption={sortOption}
+                onSortChange={handleSortOptionChange}
             />
-          ))}
-        </Tabs>
-      </Box>
-      {tabLabels.map((label, index) => (
-        <TabPanel key={index} value={value} index={index}>
-          {label}
-        </TabPanel>
-      ))}
-    </Box>
-  );
+
+            {tabLabels.map((label, index) => (
+                <TabPanel key={index} value={value} index={index}>
+                    <InfoTabPanelContent
+                        category={label}
+                        sortOption={sortOption}
+                    />
+                </TabPanel>
+            ))}
+        </Box>
+    );
 }
