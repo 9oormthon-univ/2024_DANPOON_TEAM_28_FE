6b5e6aaf90487232663ad4df8cb9108c40dc4252
LBB <202780@jnu.ac.kr>
2024-11-23 01:28:21 +0900

    Feat : 카카오 로그인 버튼 컴포넌트 생성

---
+++ src/pages/login/KakaoLoginBtn.style.ts
@@ -0,0 +1,22 @@
+import { SxProps } from "@mui/material";
+
+export const buttonStyle: SxProps = {
+  backgroundColor: "#FEDC3F",
+  color: "#000000",
+  borderRadius: "30px",
+  padding: "10px 20px",
+  height:'6%',
+  width: "88%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+export const boxStyle: SxProps = {
+  display: "inline-flex",
+  alignItems: "center",
+};
+
+export const iconStyle: SxProps = {
+  marginRight: "5px",
+};
