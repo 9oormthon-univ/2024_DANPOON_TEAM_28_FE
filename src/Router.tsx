26452594dcb3b1723d735256e8b64ffa3f063837
LBB <202780@jnu.ac.kr>
2024-11-23 04:12:22 +0900

    Feat : 초기 로딩 시 스플래시 화면 렌더링

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,59 +1,65 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 import {
-    MissionPage,
-    StatusPage,
-    MapPage,
-    MyPage,
-    NotFound,
+  MissionPage,
+  StatusPage,
+  MapPage,
+  MyPage,
+  NotFound,
+  SplashPage,
 } from './pages';
 import InfoPage from './pages/info/InfoPage';
 import AlarmPage from './pages/alarm/AlarmPage';
 import LoginPage from './pages/login/LoginPage';
 
 export const RouterPath = {
-    mission: '/',
-    status: '/status',
-    map: '/map',
-    info: '/info',
-    myPage: '/my-page',
-    alarm: '/alarm',
-    login: '/login',
-    notFound: '*',
+  splash: '/',
+  mission: '/mission',
+  status: '/status',
+  map: '/map',
+  info: '/info',
+  myPage: '/my-page',
+  alarm: '/alarm',
+  login: '/login',
+  notFound: '*',
 };
 
 const router = createBrowserRouter([
-    {
-        path: RouterPath.mission,
-        element: <MissionPage />,
-    },
-    {
-        path: RouterPath.status,
-        element: <StatusPage />,
-    },
-    {
-        path: RouterPath.map,
-        element: <MapPage />,
-    },
-    {
-        path: RouterPath.info,
-        element: <InfoPage />,
-    },
-    {
-        path: RouterPath.myPage,
-        element: <MyPage />,
-    },
-    {
-        path: RouterPath.alarm,
-        element: <AlarmPage />,
-    },
-    {
-        path: RouterPath.login,
-        element: <LoginPage />,
-    },
-    {
-        path: RouterPath.notFound,
-        element: <NotFound />,
-    },
+  {
+    path: RouterPath.splash,
+    element: <SplashPage />,
+  },
+  {
+    path: RouterPath.mission,
+    element: <MissionPage />,
+  },
+  {
+    path: RouterPath.status,
+    element: <StatusPage />,
+  },
+  {
+    path: RouterPath.map,
+    element: <MapPage />,
+  },
+  {
+    path: RouterPath.info,
+    element: <InfoPage />,
+  },
+  {
+    path: RouterPath.myPage,
+    element: <MyPage />,
+  },
+  {
+    path: RouterPath.alarm,
+    element: <AlarmPage />,
+  },
+  {
+    path: RouterPath.login,
+    element: <LoginPage />,
+  },
+  {
+    path: RouterPath.notFound,
+    element: <NotFound />,
+  },
 ]);
 
 export default router;
--- src/pages/SplashPage.tsx
+++ src/pages/SplashPage.tsx
@@ -1,7 +1,27 @@
 import Logo from "@/components/Logo";
+import { useEffect,} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from "@/Router";
 import { Box } from "@mui/material";
 
 const SplashPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkTokenAndRedirect = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      
+      const token = localStorage.getItem('token');
+      if (token) {
+        navigate(RouterPath.mission); // 로그인 상태 → 미션 페이지로 이동
+      } else {
+        navigate(RouterPath.login); // 비로그인 상태 → 로그인 페이지로 이동
+      }
+    };
+
+    checkTokenAndRedirect();
+  }, [navigate]);
+
     return (
       <Box
         sx={{
