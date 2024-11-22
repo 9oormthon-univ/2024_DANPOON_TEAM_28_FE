6eae498e825c451bf08293aa63670b4f3266a21c
LBB <202780@jnu.ac.kr>
2024-11-22 16:40:31 +0900

    Feat : 스토리지 유틸 함수 추가 및 인증 관련 저장소 초기화

---
+++ src/utils/storage/index.ts
@@ -0,0 +1,28 @@
+const initStorage = <T extends keyof StorageKey>(key: T, storage: Storage) => {
+    const storageKey = `${key}`;
+
+    const get = (): StorageKey[T] => {
+        const value = storage.getItem(storageKey);
+
+        return JSON.parse(value as string);
+    };
+    const set = (value: StorageKey[T]) => {
+        if (value == undefined || value == null) {
+            return storage.removeItem(storageKey);
+        }
+
+        const stringifiedValue = JSON.stringify(value);
+
+        storage.setItem(storageKey, stringifiedValue);
+    };
+
+    return { get, set };
+};
+
+export const authSessionStorage = initStorage('authToken', sessionStorage);
+export const authLocalStorage = initStorage('accessToken', localStorage);
+
+interface StorageKey {
+    authToken?: string;
+    accessToken?: string;
+}
